95c071921618c614fcebcdf58d6822a578eb1067
TimWidl94 <TimNowak195@gmail.com>
2024-04-25 05:31:36 +0200

    update my skills component

--- portfolio/src/app/about-me/about-me.component.ts
+++ portfolio/src/app/about-me/about-me.component.ts
@@ -5,10 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
-
-introduction = "Hi, I am a Front-end developer based in Erding near Munic, Germany. I am happy that you visited my site to look about my skills and see how i can bring your company forwards with you";
-
+  introduction =
+    'Hi, I am a Front-end developer based in Erding near Munic, Germany. I am happy that you visited my site to look about my skills and see how i can bring your company forwards with you.';
 }
--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjektsComponent } from './projekts/projekts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingpageComponent, AboutMeComponent, MySkillsComponent,],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingpageComponent, AboutMeComponent, MySkillsComponent, ProjektsComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
